e7b77a324ce9641ba7a89e8547cedb6b972fa0e7
Ruslan <kovalyovruslan@gmail.com>
2025-01-07 13:05:31 +0200

    Add terms and privacy policy page; update footer links and styles

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path('contacts', views.contacts, name='contacts'),
+    path('terms_and_privacy', views.terms_and_privacy, name='terms_and_privacy'),
 ]
--- main/views.py
+++ main/views.py
@@ -7,4 +7,15 @@ def home(request):
     return render(request, 'main/home.html', {'categories': categories})
 
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    title = 'Contact Us'
+    return render(request, 'main/contacts.html', {'title': title})
+
+def terms_and_privacy(request):
+    title = 'Terms and Privacy Policy'
+    return render(request, 'main/terms_and_privacy.html', {'title': title})
+
+# def opensource_license(request):
+#     return render(request, 'main/opensource_license.html')
+
+# def about(request):
+#     return render(request, 'main/about.html')
