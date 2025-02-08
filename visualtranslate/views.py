6a0d3661d3a76fa49b448f554a2afd2ad278a546
Ruslan <kovalyovruslan@gmail.com>
2025-02-08 22:15:31 +0200

    feat: implement visual translation feature with user input and styling

--- visualtranslate/views.py
+++ visualtranslate/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# hello world html
 def visualtranslate(request):
-    return render(request, 'visualtranslate/visualtranslate.html')
+    word = request.GET.get("word", "")  # Get word from user input (if any)
+    return render(request, "visualtranslate/visualtranslate.html", {"word": word})
